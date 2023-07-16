088488e3d6a16ea5119276a5cef92cbdca9cb062
Siddhant Goyal <siddhantgoyal@Siddhants-MacBook-Pro.local>
2023-07-17 01:52:19 +0530

    Delete Or Add Item In List Implementation

--- app/src/main/java/com/example/viewmodels/ChangeBackgroundViewModel.kt
+++ app/src/main/java/com/example/viewmodels/ChangeBackgroundViewModel.kt
@@ -1,39 +1,9 @@
 package com.example.viewmodels
 
-import android.net.Uri
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 
 class ChangeBackgroundViewModel: ViewModel() {
-
-    var backgroundColor by mutableStateOf(Color.White)
-        private set
-
-
-    fun changeBackgroundColor(){
-        backgroundColor = Color.Red
-    }
-
-    var uri: Uri? by mutableStateOf(null)
-        private set
-
-    fun updateUri(uri: Uri?){
-        this.uri = uri
-    }
-
-    var counter by mutableStateOf(0)
-        private set
-
-
-    fun incrementCount(counter: Int){
-        this.counter = counter+1
-    }
-
-
 
     var itemList = mutableStateListOf("Siddhant", "Nikhil", "Adarsh", "Sahil")
         private set
